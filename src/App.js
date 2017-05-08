6204f72fda97341aecd76c35f0f11175754ae11d
delvinlow <delvinlowzy@gmail.com>
2017-05-08 18:00:35 +0800

    Install Firebase module

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 import { View, Text } from 'react-native';
 import { Header, Button, Card, CardSection } from './components/common';
+import { apiKey, authDomain, databaseURL, projectId, storageBucket, messagingSenderId } from './Constants';
 
 class App extends Component {
+	componentWillMount() {
+		const config = {
+			apiKey,
+			authDomain,
+			databaseURL,
+			projectId,
+			storageBucket, 
+			messagingSenderId
+			};
+		firebase.initializeApp(config);
+	}
+
 	render() {
 		return (
 			<View>
