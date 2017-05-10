00ef05091861d494466b73b3e83f02117230a9d7
delvinlow <delvinlowzy@gmail.com>
2017-05-11 01:19:56 +0800

    Add conditional rendering when sign in successfully

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import firebase from 'firebase';
 import { View } from 'react-native';
-import { Header } from './components/common';
+import { Header, Spinner, Button, CardSection } from './components/common';
 import { apiKey, authDomain, databaseURL, projectId, storageBucket, messagingSenderId } from './Constants';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+	state = { loggedIn: null };
+	
 	componentWillMount() {
 		const config = {
 			apiKey,
@@ -16,13 +18,38 @@ class App extends Component {
 			messagingSenderId
 		};
 		firebase.initializeApp(config);
+
+		firebase.auth().onAuthStateChanged((user) => {
+			if (user) {
+				this.setState({ loggedIn: true });
+			} else {
+				this.setState({ loggedIn: false });
+			}
+		});
+	}
+
+	renderContent() {
+		switch (this.state.loggedIn) {
+			case true:
+				return (
+					<CardSection>
+						<Button onPress={() => firebase.auth().signOut()}>
+							Log Out
+						</Button>
+					</CardSection>
+				);
+			case false:
+				return <LoginForm />;
+			default:
+				return <Spinner size='large' />;
+		}
 	}
 
 	render() {
 		return (
 			<View>
 				<Header headerTitle="Authentication" />
-				<LoginForm />
+					{this.renderContent()}
 			</View>
 		);
 	}
